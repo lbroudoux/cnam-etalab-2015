320a8f69bd47c735a2a55046c61069c5aa98aca5
lbroudoux <laurent.broudoux@gmail.com>
2015-01-26 14:35:12 +0100

    Adding NatureRepository

---
+++ cnam/src/main/java/fr/covea/hackdds/repository/NatureRepository.java
@@ -0,0 +1,10 @@
+package fr.covea.hackdds.repository;
+
+import fr.covea.hackdds.domain.Nature;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+/**
+ * @author laurent
+ */
+public interface NatureRepository extends MongoRepository<Nature, String> {
+}
