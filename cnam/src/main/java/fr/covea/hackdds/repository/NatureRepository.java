a8e5bb2587def0dfd64caf98d722557ced729f08
yanngv29 <guillerm.yann@wanadoo.fr>
2015-01-29 21:14:49 +0100

    ajout d'un regroupement des prestations

--- cnam/src/main/java/fr/covea/hackdds/repository/NatureRepository.java
+++ cnam/src/main/java/fr/covea/hackdds/repository/NatureRepository.java
@@ -1,10 +1,15 @@
 package fr.covea.hackdds.repository;
 
+import java.util.List;
+
 import fr.covea.hackdds.domain.Nature;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 /**
  * @author laurent
  */
 public interface NatureRepository extends MongoRepository<Nature, String> {
+	
+	List<Nature> findByGroupePrestation(String groupePrestation);
 }
